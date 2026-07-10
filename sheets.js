92c1f30094b9d7f2bddf7173315e9c88f048a198
sanches6969 <sasha.goncharov.01@gmail.com>
2026-07-10 23:23:24 +0300

    Delete sheets.js

--- sheets.js
+++
@@ -1,6 +0,0 @@
-async function getState() { return null; }
-async function saveState(state) { return true; }
-async function addTrade(trade) { return true; }
-async function updateSummary(pnl) { return true; }
-
-module.exports = { getState, saveState, addTrade, updateSummary };
